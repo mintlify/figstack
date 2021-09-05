3d2d0a3772f524fcde22c10486bba353e4c2e42b
Han Wang <hyw2@cornell.edu>
2021-09-05 15:21:40 -0400

    Revert to IS_DEV = false

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const IS_DEV = true;
+const IS_DEV = false;
 export const FRONTEND_ENDPOINT = IS_DEV ? 'http://localhost:3000' : 'https://figstack.com';
 export const BACKEND_ENDPOINT = IS_DEV ? 'http://localhost:5000' : 'https://api.figstack.com';
 
